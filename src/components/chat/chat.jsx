1ce9bf7bea6c2d2032a7b57957ab8092440ff24f
Alexandre lopes de sousa <alexandrelsousa@outlook.com>
2023-11-24 21:47:22 -0300

    main
    iniciando o projeto em ReactJS.

---
+++ src/components/chat/chat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function chat() {
+  return (
+    <div>
+        <h1>Chat</h1>
+        <input type="text" placeholder='Nome de Usuário'/>
+        <button>Enviar</button>
+    </div>
+  )
+}
---
+++ src/components/join/join.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function join() {
+  return (
+    <div>
+        <h1>Join</h1>
+        <input type="text" placeholder='Nome de Usuário'/>
+        <button>Entrar</button>
+    </div>
+  )
+}
