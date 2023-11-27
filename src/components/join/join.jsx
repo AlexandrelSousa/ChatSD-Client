f4b5e98a0f2ee1afa814c7720154d7f7eda274f6
Alexandre lopes de sousa <alexandrelsousa@outlook.com>
2023-11-27 16:50:25 -0300

    main
    Configurações adicionais dos primeiros componentes da view e iniciando as configurações da conexão com o backend.

--- src/components/join/join.jsx
+++ src/components/join/join.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import io from 'socket.io-client'
 
-export default function join() {
+export default function join({setChatVisibility}) {
+
+  const userNameRef = useRef()
+
+  const handleSubmit = () => {
+    const username = userNameRef.current.value
+    if(!username.trim()) return
+    const socket = io.connect('http://localhost:3003')
+    setChatVisibility(true)
+  }
+
   return (
     <div>
         <h1>Join</h1>
-        <input type="text" placeholder='Nome de Usuário'/>
-        <button>Entrar</button>
+        <input type="text" ref={userNameRef} placeholder='Nome de Usuário'/>
+        <button onClick={()=>handleSubmit()}>Entrar</button>
     </div>
   )
 }
